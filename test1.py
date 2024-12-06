b50dd1b5d6cb4e855014a6194857b24d122c3262
gude <gudinaasefa19@gmail.com>
2024-12-07 02:28:47 +0300

    m

--- test1.py
+++ test1.py
@@ -10,6 +10,7 @@ driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()
 
 class BankProject():
     def login_test(self):
+        
         driver.get(config.base_url)
         user_id= driver.find_element(By.XPATH , ("//input[@name='uid']"))
         user_id.send_keys(config.id)
@@ -22,7 +23,7 @@ class BankProject():
         time.sleep(2)
 
 
-        driver.close()
+        #driver.close()
 
 demo=BankProject()
 demo.login_test()
